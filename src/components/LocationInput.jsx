005b244ca1ffc3b459c59150d200eda9e74d1e23
Conrad <conradmax5@gmail.com>
2024-05-30 13:51:47 +0300

    Changes

--- src/components/LocationInput.jsx
+++ src/components/LocationInput.jsx
@@ -1,30 +1,39 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Cards from "./Cards";
+import GetWeather from "./GetWeather";
 const LocationInput = () => {
-  const [location, setLocation] = useState("");
-  const getWeather = (e) => {
-    setLocation(e.target.value);
-    // <Cards location={location} />;
+  const [location, setLocation] = useState("Nairobi");
+  const getLocation = (e) => {
+    e.preventDefault();
+    let loc = document.getElementById("locationInput").value;
+    setLocation((l) => loc);
   };
+
   return (
     <div className="row justify-content-center mt-3 mb-5">
       <div className="col-auto">
-        <div className="input-group mb-3">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="Search City"
-            value={location}
-            onChange={getWeather}
-          />
-          <button
-            className="btn btn-success"
-            type="button"
-            onClick={getWeather}
-          >
-            Search
-          </button>
-        </div>
+        <form action="" method="POST">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search City"
+              defaultValue={location}
+              id="locationInput"
+              // onChange={getLocation}
+            />
+            <button
+              className="btn btn-success"
+              type="button"
+              onClick={getLocation}
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="row justify-content-center">
+        <GetWeather location={location} />
       </div>
     </div>
   );
